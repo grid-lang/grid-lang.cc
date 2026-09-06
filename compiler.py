fc90e228f84480723126d2ae8410e41237ceb59d
jido <denisgithub@bredelet.com>
2026-09-06 13:19:17 +0100

    Missed import

--- compiler.py
+++ compiler.py
@@ -9,7 +9,7 @@ from array_handler import ArrayHandler
 from utils import col_to_num, split_cell, offset_cell, validate_cell_ref, object_public_keys, public_object_view, format_display_value, iter_interpolation_placeholders, is_address, parse_address, indices_to_address, _ADDRESS_FRAGMENT, is_sparse_array, strip_array_cell_indices
 from scope import Scope, _GridStore
 from units import (
-    UNIT_ERROR, UNIVERSAL_ZERO, TYPE_ERROR, UnitValue, ConstraintError, error_value,
+    UNIT_ERROR, UNIVERSAL_ZERO, TYPE_ERROR, VALUE_ERROR, UnitValue, ConstraintError, error_value,
     is_error_value, strip_units, register_conversion, has_conversion,
     lookup_conversions,
 )
